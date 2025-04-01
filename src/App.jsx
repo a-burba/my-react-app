afbe3906afc52757418c60cc4538a9aed6c11707
a-burba <a.burba@wustl.edu>
2025-04-01 16:55:34 -0500

    added arrow functions

--- src/App.jsx
+++ src/App.jsx
@@ -36,19 +36,20 @@ export default function Board() { //The first line defines a function called Squ
   return (
     <>
        <div className="board-row">
-        <Square value={squares[0]} onSquareClick={handleClick(0)} /> {/* this is what's causing the error bc it's re-rendering too much*/}
-        <Square value={squares[1]} />
-        <Square value={squares[2]} />
+        {/* () => handleClick(0) is an arrow function, which is a shorter way to define functions. When the square is clicked, the code after the => “arrow” will run, calling handleClick(0). */}
+        <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
+        <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
+        <Square value={squares[2]} onSquareClick={() => handleClick(2)} />
       </div>
       <div className="board-row">
-        <Square value={squares[3]} />
-        <Square value={squares[4]} />
-        <Square value={squares[5]} />
+        <Square value={squares[3]} onSquareClick={() => handleClick(3)} />
+        <Square value={squares[4]} onSquareClick={() => handleClick(4)} />
+        <Square value={squares[5]} onSquareClick={() => handleClick(5)} />
       </div>
       <div className="board-row">
-        <Square value={squares[6]} />
-        <Square value={squares[7]} />
-        <Square value={squares[8]} />
+        <Square value={squares[6]} onSquareClick={() => handleClick(6)} />
+        <Square value={squares[7]} onSquareClick={() => handleClick(7)} />
+        <Square value={squares[8]} onSquareClick={() => handleClick(8)} />
       </div>
     </>
   );
