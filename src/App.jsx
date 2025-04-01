6400f73d23a8c9872734916fefc24226464c5b07
a-burba <a.burba@wustl.edu>
2025-04-01 16:48:31 -0500

    onClick hardcoded for squares[0]

--- src/App.jsx
+++ src/App.jsx
@@ -6,20 +6,37 @@
  import { useState } from 'react';
 
 
-function Square({value}) {
-  return <button className="square">{value}</button>;
+ // props
+function Square({ value, onSquareClick }) {
+  // state is private to the parent, so need to call onSqareClick instead of updating directly in Sqaure()
+  return (
+    <button className="square" onClick={onSquareClick}>
+      {value}
+    </button>
+  );
 }
 
 export default function Board() { //The first line defines a function called Square. The export JavaScript keyword makes this function accessible outside of this file. The default keyword tells other files using your code that it’s the main function in your file.
   // lifting state into the parent
   const [squares, setSquares] = useState(Array(9).fill(null));
 
+  
+  function handleClick() {
+    // creates a copy of the squares array
+    const nextSquares = squares.slice();
+    // updates the nextSquares array to add X to the first ([0] index) square.
+    nextSquares[0] = "X";
+    // Calling the setSquares function lets React know the state of the component has changed. This will trigger a re-render of the components that use the squares state (Board) as well as its child components (the Square components that make up the board).
+    setSquares(nextSquares);
+  }
+
+
   // component needs to return a SINGLE jsx element 
   // use fragments to wrap multiple things instead
   return (
     <>
        <div className="board-row">
-        <Square value={squares[0]} />
+        <Square value={squares[0]} onSquareClick={handleClick} />
         <Square value={squares[1]} />
         <Square value={squares[2]} />
       </div>
